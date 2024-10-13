bbdc46198cfeb11dbc3eccc4e3a05090e5c06379
GEO-HACK <geoffreykirumba0@gmail.com>
2024-10-13 10:03:55 +0300

    Get items by id

--- myapp/views.py
+++ myapp/views.py
@@ -3,14 +3,10 @@ from django.http import JsonResponse
 from .models import Drinks
 from .serializers import DrinkSerializer
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework import status
- 
-
 
 @api_view(['GET', 'POST'])
 def drink_list(request):
- 
     if request.method == 'GET':
         drinks = Drinks.objects.all()
         serializer = DrinkSerializer(drinks, many=True)
@@ -18,10 +14,24 @@ def drink_list(request):
         return JsonResponse(serializer.data, safe=False)
 
     if request.method == 'POST':
-        serializer = DrinkSerializer(data= request.data) 
+        serializer = DrinkSerializer(data= request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)    
 
 
+def drink_detail(request, id):
 
+    try:
+
+       drinks = Drinks.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status= status.HTTP_404_NOTFOUND)
+
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drinks )
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        pass
+    elif request.method == 'DELETE':
+        pass        
